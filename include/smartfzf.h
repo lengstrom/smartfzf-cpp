b03f9754b925b9d73a5865715ee6c62c9f0e7eea
lengstrom <lengstrom@users.noreply.github.com>
2015-07-29 23:52:53 -0400

    finsihed merging tests (havent tested yet tho)

--- include/smartfzf.h
+++ include/smartfzf.h
@@ -1,13 +1,4 @@
 #ifndef SMARTFZF_INCLUDE_SMARTFZF_H_ // include guard
 #define SMARTFZF_INCLUDE_SMARTFZF_H_
 
-#include <string>
-
-class Test {
-public:
-    Test(int);
-    ~Test();
-    bool fz_match(const std::string &, const std::string &);
-}; // semicolon!
-
 #endif // SMARTFZF_INCLUDE_SMARTFZF_H_
--- include/tests.h
+++ include/tests.h
@@ -1,8 +1,14 @@
 #ifndef GUARD_Test
 #define GUARD_Test
+
+#include <string>
+
 class Test {
 public:
     Test(int);
     ~Test();
+    bool fz_match(const std::string &, const std::string &);
 }; // semicolon!
+
+
 #endif
