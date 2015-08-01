d823451cf54c912cbc3f1455e11f7b92ef1e80be
lengstrom <lengstrom@users.noreply.github.com>
2015-08-01 18:59:24 -0400

    added match_from_candidates function

--- src/smartfzf.cc
+++ src/smartfzf.cc
@@ -2,24 +2,38 @@
 #include <iterator>
 #include <algorithm>
 #include <iostream>
+#include <vector>
+
+using std::string;
+using std::vector;
 
 /* returns true if the first string
  * is a lazy subset of the second
  * string */
-bool fz_match(const std::string &input, const std::string &against)
-{
-    for(std::string::const_iterator input_iter = input.begin(), 
+bool fz_match(const string &input, const string &against) {
+    for (string::const_iterator input_iter = input.begin(), 
             against_iter = against.begin(); 
             against_iter != against.end();
             against_iter++) {
-        // std::cout << "chars: " << *input_iter << " against " << *against_iter << '\n';
-        if(tolower(*input_iter) == tolower(*against_iter)) {
+
+        if (tolower(*input_iter) == tolower(*against_iter)) {
             input_iter++;
-            if( input_iter == input.end() )
+            if (input_iter == input.end())
                 return true;
         }
     }
+
     return false;
 }
 
-// do stuff
+vector<string> match_from_candidates(const string &input, vector<string> &candidates) {
+    vector<string> results;
+    for (vector<string>::const_iterator it = candidates.begin(); it != v.end(); it++) {
+        const string &curr = *it;
+        if (fz_match(input, curr)) {
+            results.push_back(curr);
+        }
+    }
+
+    return results;
+}
