bed02a2cea44153242622a1b18fed171425969f9
lengstrom <lengstrom@users.noreply.github.com>
2015-08-02 23:49:58 -0400

    started adding prefix matching func

--- src/smartfzf.cc
+++ src/smartfzf.cc
@@ -3,18 +3,22 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using std::string;
+using std::map;
 using std::vector;
+
+/* Eventually add caching */
 
 /* returns true if the first string
  * is a lazy subset of the second
  * string */
 bool fz_match(const string &input, const string &against) {
     for (string::const_iterator input_iter = input.begin(), 
-            against_iter = against.begin(); 
-            against_iter != against.end();
-            against_iter++) {
+             against_iter = against.begin(); 
+         against_iter != against.end();
+         against_iter++) {
 
         if (tolower(*input_iter) == tolower(*against_iter)) {
             input_iter++;
@@ -25,6 +29,8 @@ bool fz_match(const string &input, const string &against) {
 
     return false;
 }
+
+
 /* Given an input string to match to and a vector of candidates (strings) to match to
  * Returns vector of strings that matched using fuzzy_search 
  */
@@ -39,4 +45,13 @@ vector<string> match_from_candidates(const string &input, vector<string> &candid
     }
 
     return results;
+}
+
+bool prefix_match(const string &input, const string &against) {
+    for (string::const_iterator input_iter = input.begin(), against_iter = against.begin();
+         against_iter != against.end();
+         against_iter++, input_iter++;
+         ) {
+        
+    }
 }
