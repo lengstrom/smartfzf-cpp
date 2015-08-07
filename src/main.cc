f0fbde6ce245aa8ee70b2e2ce4cf8521a343bd20
lengstrom <lengstrom@users.noreply.github.com>
2015-08-07 00:41:20 -0400

    readded test lines

--- src/main.cc
+++ src/main.cc
@@ -11,9 +11,9 @@ int main(int argc, char *argv[]) {
     vector<string> lines_to_render;
     lines_to_render.push_back("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium");
     lines_to_render.push_back("o enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut ");
-    // lines_to_render.push_back("dipisci velit, sed quia non numquam eius modi tempora in");
-    // lines_to_render.push_back("consequatur, vel illum qui dolorem eum fugiat quo volupta");
-    // lines_to_render.push_back("consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore");
+    lines_to_render.push_back("dipisci velit, sed quia non numquam eius modi tempora in");
+    lines_to_render.push_back("consequatur, vel illum qui dolorem eum fugiat quo volupta");
+    lines_to_render.push_back("consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore");
 
     const vector<string> &ref = lines_to_render;
 
