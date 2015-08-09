1ac39c748fadf976513f68e3b00272d0bedb638c
lengstrom <lengstrom@users.noreply.github.com>
2015-08-09 17:49:06 -0400

    compiles!!

--- src/fs.cc
+++ src/fs.cc
@@ -6,7 +6,7 @@
 using std::vector;
 using namespace boost::filesystem;
 
-vector<path*> sorted_dir_contents(const path &dir_path) {
+vector<const path*> sorted_dir_contents(const path &dir_path) {
     /* 
      * Possible issues:
      * Permissions - unable to read into directory (Does this actually happen???)
@@ -15,10 +15,11 @@ vector<path*> sorted_dir_contents(const path &dir_path) {
 
     // For now use naive implementation assuming no errors occur
     // Research done: try catch blocks have 0 performance hit (unless there's an exception, obv)
-    vector<path*> contents;
+    vector<const path*> contents;
     directory_iterator end_itr; // default constructor = object past end
     for (directory_iterator itr(dir_path); itr != end_itr; itr++) {
-        contents.push_back(&(itr->path()));
+        const path * curr_file = &(itr->path());
+        contents.push_back(curr_file);
     }
 
     std::sort(contents.begin(), contents.end());
@@ -26,42 +27,43 @@ vector<path*> sorted_dir_contents(const path &dir_path) {
 }
 
 // recursively copy dir contents
-vector<path> recursive_sorted_dir_contents(const path &dir_path) {
-    vector<path*> appended_contents;
-    vector<path*> sorted_contents;
+vector<const path*> recursive_sorted_dir_contents(const path &dir_path) {
+    vector<const path*> appended_contents;
+    vector<const path*> sorted_contents;
 
     directory_iterator end_itr;
     for (directory_iterator itr(dir_path); itr != end_itr; itr++) {
-        path * curr_path = &(itr->path()); // Must be a better way of doing this
+        const path * curr_path = &(itr->path()); // Must be a better way of doing this
         if (is_directory(itr->status())) {
-            // way of concatenating vectors without copying objects??
-            vector<path*> curr_contents = recursive_sorted_dir_contents(curr_path);
+            // way of concatenating stdvectors without copying objects??
+            vector<const path*> curr_contents = recursive_sorted_dir_contents(*curr_path);
             // merge curr_contents into sorted_contents
-            vector<path*> tmp;
+            vector<const path*> tmp;
             tmp.reserve(sorted_contents.size() + curr_contents.size());
-
             std::merge(curr_contents.begin(), curr_contents.end(),
                        sorted_contents.begin(), sorted_contents.end(),
-                       std::back_inserter(tmp));
+                       tmp.begin());
+            sorted_contents.swap(tmp);
         } else {
             appended_contents.push_back(curr_path);
         }
     }
 
-    std::sort(appended_contents.begin(), sorted_coments.end());
+    std::sort(appended_contents.begin(), sorted_contents.end());
 
-    vector<path*> final_sorted;
+    vector<const path*> final_sorted;
+    final_sorted.reserve(appended_contents.size() + sorted_contents.size());
     std::merge(appended_contents.begin(), appended_contents.end(),
                sorted_contents.begin(), sorted_contents.end(),
                final_sorted.begin());
 
     // merge appended_contents into sorted_contents
-    return sorted_contents;
+    return final_sorted;
 }
 
-Path_Node * build_path_node(const path &dir_path) { //remember to add a parent (if applicable)
-    Path_Node * node;
-    node->dir_name = dir_path;
-    node->contents = dir_contents(dir_path);
-    return node;
-}
+// Path_Node * build_path_node(const path &dir_path) { //remember to add a parent (if applicable)
+//     Path_Node * node;
+//     node->dir_name = dir_path;
+//     node->contents = dir_contents(dir_path);
+//     return node;
+// }
