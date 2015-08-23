4efd7281f354825bf04f31659fa158d7ce040fb7
lengstrom <lengstrom@users.noreply.github.com>
2015-08-23 02:56:31 -0400

    added/fixed tests

--- test/test_filesystem.cc
+++ test/test_filesystem.cc
@@ -14,38 +14,38 @@ using namespace boost::filesystem;
 using std::vector;
 using std::string;
 
-BOOST_AUTO_TEST_CASE(get_base_directory_test)
-{
-    std::cout << "started test.." << std::endl;
-    std::string input = "./test/test_filesystem";
-    boost::filesystem::path base(".");
-    bool err = false;
-    std::vector<std::string> strs = dir_components(input, base, err);
-    std::cout << "Started vector printing..." << std::endl;
-    for (auto i : strs) {
-        std::cout << i << std::endl;
-    }
-    std::cout << "Ended vector printing." << std::endl;
+path base_dir = path("./../test/sandbox");
+
+bool check_vecs(std::vector<std::string> v1, std::vector<std::string> v2) {
+    std::vector<std::string>::size_type n = v1.size();
+    return (std::equal(v1.begin(), v1.begin() + n, v2.begin()));
 }
 
-BOOST_AUTO_TEST_CASE(filesystem_test)
-{
-    // Filesystem checking
-    path curr_dir = current_path();
-    vector<string> contents = sorted_dir_contents(curr_dir);
-    path contents_against[2] = {"sandbox", "test_smartfzf.cc"};
-    bool res = true;
-    for (int i =0 ; i < 2; i++) {
-        if (contents_against[i] != contents[i]) {
-            res = false;
-        }
-    }
+
+BOOST_AUTO_TEST_CASE(get_base_directory_test) {
+    std::cout << "started test.." << std::endl;
+    std::string input = "./a";
+    bool err = false;
+    std::vector<std::string> strs = dir_components(input, base_dir, err);
+    std::vector<std::string> strs_against = {"/","Users", "loganengstrom","projects","smartfzf-cpp","test","sandbox"};
     
+    bool res = check_vecs(strs, strs_against);
+    BOOST_CHECK(res);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(filesystem_test) {
+    // Filesystem checking
+    vector<string> contents = sorted_dir_contents(base_dir);
+    vector<string> contents_against = {"a","az","b","bb","c","dir_1","dir_2"};
+    bool res = check_vecs(contents, contents_against);
     BOOST_CHECK(res);
     BOOST_CHECK(!is_project(contents));
 
     // some kind of memory error here
-    vector<string> all_contents = recursive_sorted_contents(curr_dir);
+    // vector<string> all_contents = recursive_sorted_contents(curr_dir);
     // std::cout << "Getting all contents" << std::endl;
     // for (auto i : all_contents) {
     //     std::cout << i << std::endl;
